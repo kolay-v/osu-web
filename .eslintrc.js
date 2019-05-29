2ca92812e79fd6c620c0b98ed9c768a5ea5b062a
@kolayv <kolay.va@gmail.com>
2019-05-29 18:33:21 +0000

    improved linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,11 @@ module.exports = {
     'plugin:vue/essential',
     '@vue/standard',
     '@vue/typescript'
-  ],
+  ], /*
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
+  }, */
   parserOptions: {
     parser: '@typescript-eslint/parser'
   }
